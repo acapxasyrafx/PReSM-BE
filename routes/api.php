2eec5c3fa313943c73de538f083d3f8125abc523
AcapXasyraF <acapxasyrafx@yahoo.com>
2024-03-27 12:06:27 +0800

    added login and register api as based

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\API\ProductController;
+
+Route::controller(RegisterController::class)->group(function(){
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::resource('products', ProductController::class);
+});
+
