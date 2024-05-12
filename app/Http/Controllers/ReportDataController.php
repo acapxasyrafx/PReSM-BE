d33dd3fddc7c2e0dba4b4c4d96b3f76c1827bcb1
AcapXasyraF <acapxasyrafx@yahoo.com>
2024-05-12 09:58:10 +0800

    added report

---
+++ app/Http/Controllers/ReportDataController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
+use validator;
+
+class ReportDataController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show()
+    {
+        //
+        // return ("Api is on");
+        // $validator = Validator::make($request->all(), [
+        //     'report_name' => 'required',
+        // ]);
+
+        // if($validator->fails()){
+        //     return $this->sendError('Validation Error.', $validator->errors());
+        // }
+
+        $count = DB::table('users')->count();
+        return $count;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,15 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\API\ProductController;
 use App\Http\Controllers\API\settings\SystemSettingsController;
-
+use App\Http\Controllers\ReportDataController;
 Route::controller(RegisterController::class)->group(function(){
     Route::post('register', 'register');
     Route::post('login', 'login');
+});
+// Report
+
+Route::controller(ReportDataController::class)->group(function(){
+    Route::get('data-report','show');
 });
 
 Route::middleware('auth:sanctum')->group( function () {
@@ -25,5 +30,7 @@ Route::middleware('auth:sanctum')->group( function () {
     Route::controller(SystemSettingsController::class)->group(function(){
         Route::post('system-settings','create');
     });
+
+
 });
 
